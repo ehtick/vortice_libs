2313e5a4e1cb62b50c7e327a1529b6aee0bfd288
Amer Koleci <amerkoleci@gmail.com>
2022-06-07 15:31:59 +0200

    More bindings.

--- src/vortice_image/ktx_c_binding.cpp
+++ src/vortice_image/ktx_c_binding.cpp
@@ -8,7 +8,7 @@
 extern "C" {
 #endif
 
-    VGPU_IMAGE_API ktxTexture* ktx_load_from_memory(const uint8_t* data, uint32_t length, KTX_error_code* out_status) {
+    VGPU_IMAGE_API ktxTexture* ktx_load_from_memory(const uint8_t* data, size_t length, KTX_error_code* out_status) {
 
         KTX_error_code result;
 
@@ -33,45 +33,79 @@ extern "C" {
         return ktx->isArray;
     }
 
-    VGPU_IMAGE_API ktx_bool_t ktx_get_isCubemap(ktxTexture* ktx) {
+    VGPU_IMAGE_API ktx_bool_t ktx_get_isCubemap(ktxTexture* ktx)
+    {
         return ktx->isCubemap;
     }
 
-    VGPU_IMAGE_API ktx_bool_t ktx_get_isCompressed(ktxTexture* ktx) {
+    VGPU_IMAGE_API ktx_bool_t ktx_get_isCompressed(ktxTexture* ktx)
+    {
         return ktx->isCompressed;
     }
-    VGPU_IMAGE_API ktx_uint32_t ktx_get_baseWidth(ktxTexture* ktx) {
+
+    VGPU_IMAGE_API ktx_bool_t ktx_get_IsPremultiplied(ktxTexture* texture)
+    {
+        return (texture->classId == ktxTexture2_c
+            && ktxTexture2_GetPremultipliedAlpha((ktxTexture2*)texture));
+    }
+
+    VGPU_IMAGE_API uint32_t ktx_get_NumComponents(ktxTexture* texture)
+    {
+        if (texture->classId != ktxTexture2_c)
+        {
+            return 0;
+        }
+
+        return ktxTexture2_GetNumComponents((ktxTexture2*)texture);
+    }
+
+    VGPU_IMAGE_API uint32_t ktx_get_baseWidth(ktxTexture* ktx)
+    {
         return ktx->baseWidth;
     }
-    VGPU_IMAGE_API ktx_uint32_t ktx_get_baseHeight(ktxTexture* ktx) {
+
+    VGPU_IMAGE_API ktx_uint32_t ktx_get_baseHeight(ktxTexture* ktx)
+    {
         return ktx->baseHeight;
     }
-    VGPU_IMAGE_API ktx_uint32_t ktx_get_numDimensions(ktxTexture* ktx) {
+
+    VGPU_IMAGE_API ktx_uint32_t ktx_get_numDimensions(ktxTexture* ktx)
+    {
         return ktx->numDimensions;
     }
+
     VGPU_IMAGE_API ktx_uint32_t ktx_get_numLevels(ktxTexture* ktx) {
         return ktx->numLevels;
     }
-    VGPU_IMAGE_API ktx_uint32_t ktx_get_numLayers(ktxTexture* ktx) {
+
+    VGPU_IMAGE_API ktx_uint32_t ktx_get_numLayers(ktxTexture* ktx)
+    {
         return ktx->numLayers;
     }
-    VGPU_IMAGE_API ktx_uint32_t ktx_get_numFaces(ktxTexture* ktx) {
+
+    VGPU_IMAGE_API ktx_uint32_t ktx_get_numFaces(ktxTexture* ktx)
+    {
         return ktx->numFaces;
     }
-    VGPU_IMAGE_API ktx_uint32_t ktx_get_vkFormat(ktxTexture* ktx) {
+
+    VGPU_IMAGE_API ktx_uint32_t ktx_get_vkFormat(ktxTexture* ktx)
+    {
         if (ktx->classId != ktxTexture2_c) {
             return 0; // VK_FORMAT_UNDEFINED
         }
         return ((ktxTexture2*)ktx)->vkFormat;
     }
-    VGPU_IMAGE_API ktxSupercmpScheme ktx_get_supercompressionScheme(ktxTexture* ktx) {
+
+    VGPU_IMAGE_API ktxSupercmpScheme ktx_get_supercompressionScheme(ktxTexture* ktx)
+    {
         if (ktx->classId != ktxTexture2_c) {
             return KTX_SS_NONE;
         }
         return ((ktxTexture2*)ktx)->supercompressionScheme;
     }
 
-    VGPU_IMAGE_API ktx_uint32_t ktx_get_orientation(ktxTexture* ktx) {
+    VGPU_IMAGE_API ktx_uint32_t ktx_get_orientation(ktxTexture* ktx)
+    {
         ktx_uint32_t orientation = 0;
         if (ktx->orientation.x == KTX_ORIENT_X_LEFT) {
             orientation |= 0x1;
@@ -85,11 +119,24 @@ extern "C" {
         return orientation;
     }
 
-    VGPU_IMAGE_API ktx_uint32_t ktx_transcode(ktxTexture2* ktx, ktx_transcode_fmt_e fmt, ktx_transcode_flags transcodeFlags) {
-        return ktxTexture2_TranscodeBasis(ktx, fmt, transcodeFlags);
+    VGPU_IMAGE_API ktx_bool_t ktx_NeedsTranscoding(ktxTexture* ktx)
+    {
+        return ktxTexture_NeedsTranscoding(ktx);
     }
 
-    VGPU_IMAGE_API void ktx_get_data(ktxTexture* ktx, const uint8_t** data, size_t* length) {
+    VGPU_IMAGE_API ktx_error_code_e ktx_TranscodeBasis(ktxTexture* texture, ktx_transcode_fmt_e fmt, ktx_transcode_flags transcodeFlags)
+    {
+        if (texture->classId != ktxTexture2_c)
+        {
+            //std::cout << "ERROR: transcodeBasis is only supported for KTX2" << std::endl;
+            return KTX_INVALID_VALUE;
+        }
+
+        return ktxTexture2_TranscodeBasis((ktxTexture2*)texture, fmt, transcodeFlags);
+    }
+
+    VGPU_IMAGE_API void ktx_get_data(ktxTexture* ktx, const uint8_t** data, size_t* length)
+    {
         *data = ktx->pData;
         *length = (size_t)ktx->dataSize;
     }
