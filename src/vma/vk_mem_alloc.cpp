63ac85e2f50c947eed8fa977387c8230a2af2f95
Amer Koleci <amerkoleci@gmail.com>
2022-06-15 14:39:49 +0200

    More image loading support (Add STB). Vulkan Memory Allocator C bindings.

---
+++ src/vma/vk_mem_alloc.cpp
@@ -0,0 +1,12 @@
+// Copyright © Amer Koleci and Contributors.
+// Licensed under the MIT License (MIT). See LICENSE in the repository root for more information.
+
+#if defined(_WIN32)
+#   define VMA_CALL_PRE  __declspec(dllexport)
+#   define VMA_CALL_POST __cdecl
+#elif defined(__ANDROID__)
+#   define VMA_CALL_PRE __attribute__((visibility("default")))
+#endif
+
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
--- src/vortice_image/vortice_image.h
+++ src/vortice_image/vortice_image.h
@@ -28,9 +28,56 @@
 extern "C" {
 #endif /* __cplusplus */
 
-    typedef struct VImage VImage;
+    typedef enum ImageFormat {
+        IMAGE_FORMAT_R8,
+        IMAGE_FORMAT_RG8,
+        IMAGE_FORMAT_RGBA8,
+        IMAGE_FORMAT_R16,
+        IMAGE_FORMAT_RG16,
+        IMAGE_FORMAT_RGBA16,
+        IMAGE_FORMAT_R16F,
+        IMAGE_FORMAT_RG16F,
+        IMAGE_FORMAT_RGBA16F,
+        IMAGE_FORMAT_R32F,
+        IMAGE_FORMAT_RG32F,
+        IMAGE_FORMAT_RGBA32F,
+        IMAGE_FORMAT_RGB565,
+        IMAGE_FORMAT_RGB5A1,
+        IMAGE_FORMAT_RGB10A2,
+        IMAGE_FORMAT_RG11B10F,
+        IMAGE_FORMAT_BC1,
+        IMAGE_FORMAT_BC2,
+        IMAGE_FORMAT_BC3,
+        IMAGE_FORMAT_BC4U,
+        IMAGE_FORMAT_BC4S,
+        IMAGE_FORMAT_BC5U,
+        IMAGE_FORMAT_BC5S,
+        IMAGE_FORMAT_BC6UF,
+        IMAGE_FORMAT_BC6SF,
+        IMAGE_FORMAT_BC7,
+        IMAGE_FORMAT_ASTC_4x4,
+        IMAGE_FORMAT_ASTC_5x4,
+        IMAGE_FORMAT_ASTC_5x5,
+        IMAGE_FORMAT_ASTC_6x5,
+        IMAGE_FORMAT_ASTC_6x6,
+        IMAGE_FORMAT_ASTC_8x5,
+        IMAGE_FORMAT_ASTC_8x6,
+        IMAGE_FORMAT_ASTC_8x8,
+        IMAGE_FORMAT_ASTC_10x5,
+        IMAGE_FORMAT_ASTC_10x6,
+        IMAGE_FORMAT_ASTC_10x8,
+        IMAGE_FORMAT_ASTC_10x10,
+        IMAGE_FORMAT_ASTC_12x10,
+        IMAGE_FORMAT_ASTC_12x12,
 
-    VGPU_IMAGE_API VImage* vimage_load_ktx(const uint8_t* data, size_t size);
+        IMAGE_FORMAT_COUNT,
+        _ImageFormat_Force32 = 0x7FFFFFFF
+    } ImageFormat;
+
+    typedef struct Image Image;
+
+    VGPU_IMAGE_API uint32_t vimageGetMemorySize(ImageFormat format, uint32_t width, uint32_t height);
+    VGPU_IMAGE_API Image* vimageCreateFromMemory(const uint8_t* data, size_t size);
 
 #ifdef __cplusplus
 }
